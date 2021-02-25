8f5681e65250697ea2348becf8a5f891b669255a
Loc Nguyen <>
2021-02-25 15:43:16 +0700

    first

---
+++ src/components/Create/Create.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const App = props => {
+    return (
+        <div>
+            <p>{props.aa}</p>
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/People/People.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const App = () => {
+    return (
+        <div>
+            <p>this is people</p>
+        </div>
+    )
+}
+
+export default App
